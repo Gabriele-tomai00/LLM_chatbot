ce4188fa178b1e6cc3b1ef8787ecf5d486191f24
Gabriele <tomaigabriele@gmail.com>
2025-12-28 12:54:57 +0100

    fix: split_jsonl, now works

--- split_jsonl.py
+++ split_jsonl.py
@@ -2,19 +2,24 @@
 import sys
 import os
 from tqdm import tqdm
+import shutil
 
 CHUNK_SIZE = 8 * 1024 * 1024 * 1024  # 8GB
 
 def split_jsonl(input_path, output_dir):
     if not os.path.isfile(input_path):
         raise FileNotFoundError(f"Input file not found: {input_path}")
-    os.makedirs(output_dir, exist_ok=True)
 
+    # Delete folder if exists
     if os.path.exists(output_dir) and os.path.isdir(output_dir):
         shutil.rmtree(output_dir)
         print(f"Folder '{output_dir}' removed.")
 
+    # Recreate folder
+    os.makedirs(output_dir, exist_ok=True)
+
     part = 1
+
     bytes_written = 0
     out_file = open(os.path.join(output_dir, f"part_{part}.jsonl"), "wb")
 
@@ -39,8 +44,5 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python split_jsonl.py <input_file> <output_dir>")
         sys.exit(1)
-
-
-
 
     split_jsonl(sys.argv[1], sys.argv[2])
